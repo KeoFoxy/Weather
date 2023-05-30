ddc5a1eb95ff44f7461a9a71a519beab27e7721d
KeoFoxy <arasdiel68@gmail.com>
2023-05-30 20:01:04 +0300

    app init

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Alexander Sorokin on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
