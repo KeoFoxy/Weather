3f019c537b1c690ed6cb6521e991a4ebf2c3ac77
KeoFoxy <arasdiel68@gmail.com>
2023-05-30 20:45:50 +0300

    City, Icon, degree and Background

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            skyBackground
+                .edgesIgnoringSafeArea(.all)
+            City
         }
-        .padding()
+    }
+    
+    var City: some View {
+        VStack {
+            Text("Cupertino, CA")
+                .font(.system(size: 32, weight: .medium, design: .default))
+                .foregroundColor(.white)
+                .padding()
+            
+            VStack(spacing: 10) {
+                Image(systemName: "cloud.sun.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 180, height: 180)
+                
+                Text("76°")
+                    .font(.system(size: 70, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                
+            }
+            Spacer()
+        }
     }
 }
 
